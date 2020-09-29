390ef5956dec0faf7d771f4239ada4783351c6ab
MandasK <king.amanda.r@gmail.com>
2020-09-29 16:52:35 -0500

    first commit

---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,155 @@
+﻿using CoffeeShop.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT c.Id, c.Title, c.BeanVarietyId, bv.Name AS 'BeanVariety'
+                            FROM Coffee c
+                            JOIN BeanVariety bv ON bv.Id = c.BeanVarietyId
+                            WHERE Flag_Delete = 0
+                                        ";
+                    var reader = cmd.ExecuteReader();
+                    var coffees = new List<Coffee>();
+                    while (reader.Read())
+                    {
+                        var coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            BeanVarietyName = reader.GetString(reader.GetOrdinal("BeanVariety"))
+                        };
+                        coffees.Add(coffee);
+                    }
+                    reader.Close();
+
+                    return coffees;
+                }
+            }
+        }
+
+        public Coffee Get(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id, c.Title, bv.Name AS 'BeanVariety'
+                        FROM Coffee c
+                        JOIN BeanVariety bv ON bv.Id = c.BeanVarietyId
+                        WHERE c.Id = @id
+                        AND Flag_Delete = 0
+                                        ";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    Coffee coffee = null;
+                    if(reader.Read())
+                    {
+                        coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            BeanVarietyName = reader.GetString(reader.GetOrdinal("BeanVariety"))
+                        };
+                    }
+                    reader.Close();
+                    return coffee;
+                }
+            }
+        }
+
+        public void Add(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Coffee (Id, Title, BeanVarietyId)
+                        OUTPUT INSERTED.ID
+                        VALUES(@id, @title, @beanVarietyId)
+                                      ";
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVarietyId);
+
+                    coffee.Id = (int)cmd.ExecuteScalar();
+
+                }
+            }
+        }
+
+        public void Update(Coffee coffee)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Coffee
+                        SET Title = @title,
+	                        BeanVarietyId = @beanVarietyId
+                        WHERE Id = @id
+                                      ";
+                    cmd.Parameters.AddWithValue("@id", coffee.Id);
+                    cmd.Parameters.AddWithValue("@title", coffee.Title);
+                    cmd.Parameters.AddWithValue("@beanVarietyId", coffee.BeanVarietyId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using(var conn = Connection)
+            {
+                conn.Open();
+                using(var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Coffee
+                        SET Flag_Delete = 1
+                        WHERE Id = @id
+                                      ";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Repositories/ICoffeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShop.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface ICoffeeRepository
+    {
+        Coffee Get(int id);
+        List<Coffee> GetAll();
+        void Update(Coffee coffee);
+        void Add(Coffee coffee);
+        void Delete(int id);
+    }
+}
