75b5ec3aec757571b1ecd7f41b34b0dfc1ea60bb
MandasK <king.amanda.r@gmail.com>
2020-09-30 11:52:25 -0500

    Chapter 2 complete

--- js-app/main.js
+++ js-app/main.js
@@ -2,12 +2,56 @@ const url = "https://localhost:5001/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
-    getAllBeanVarieties()
-        .then(beanVarieties => {
-            console.log(beanVarieties);
-        })
+    getAllBeanVarieties().then(beanVarieties => {
+        const beans = document.getElementById('display-bean-varieties')
+        beans.innerHTML = renderBeanHTML(beanVarieties)
+    })
 });
 
+const addBean = document.getElementById('addNewBean')
+addBean.addEventListener('click', event => {
+    event.preventDefault()
+
+    const newBean = {
+        name: document.getElementById('bname').value,
+        region: document.getElementById('bregion').value,
+        notes: document.getElementById('bnotes').value
+    }
+
+    addNewBean(newBean).then(() => {
+        document.getElementById('bname').value = ""
+        document.getElementById('bregion').value = ""
+        document.getElementById('bnotes').value = ""
+    })
+
+})
+
+
+function renderBeanHTML(beans) {
+    htmlToRender = `<h3>Coffee Bean Varieties</h3>
+                    <div>`
+    beans.forEach(bean => {
+        if (bean.notes === null) bean.notes = "No Notes"
+        htmlToRender += `<h3>Name: ${bean.name}</h3> 
+                        <h4>Region: ${bean.region}</h4>
+                        <p> Notes: ${bean.notes} </p>`
+    })
+    htmlToRender += '</div>'
+    return htmlToRender
+}
+
+
+
 function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+    return fetch(url).then(resp => resp.json())
+}
+
+function addNewBean(bean) {
+    return fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(bean)
+    }).then(res => res.json())
 }
